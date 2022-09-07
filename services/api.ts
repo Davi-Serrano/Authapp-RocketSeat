af59531feb2116765e6891d701b3c4577a37666c
Davi Serrano <daviscardoso2901@gmail.com>
2022-09-07 13:38:59 -0300

    conectando com a api

--- services/api.ts
+++ services/api.ts
@@ -10,7 +10,7 @@ export function setupApiClient(ctx: undefined){
     let cookies = parseCookies(ctx);
 
      const api = axios.create({
-        baseURL: "http://localhost:3333",
+        baseURL: "https://lndback.herokuapp.com",
         headers: {
             Authorization: `Bearer ${cookies["nextauth.token"]}`
         }
